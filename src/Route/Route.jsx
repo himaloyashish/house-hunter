02715063ff8bd38e8b92463eb8b421857ecf5acf
himaloyash <himaloyashis98@gmail.com>
2024-01-23 15:52:47 +0600

    sign in and signUp created

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Page/Home/Home";
+import SignUp from "../shared/SignUp/SignUp";
+import SignIn from "../shared/SignIn/SignIn";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +13,14 @@ const router = createBrowserRouter([
                 path:'/',
                 element:<Home></Home>,
             },
+            {
+                path:"signUp",
+                element:<SignUp></SignUp>
+            },
+            {
+                path:"signIn",
+                element:<SignIn></SignIn>
+            }
         ]
     }
 ]);
