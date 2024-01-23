9d21a58ff35ffc8fc58ca2b1106acdd28a732a27
himaloyash <himaloyashis98@gmail.com>
2024-01-23 15:38:29 +0600

    sign in and sign Up page created

---
+++ src/shared/SignIn/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <div>
+            <p>This is signIn Page</p>
+        </div>
+    );
+};
+
+export default SignIn;
---
+++ src/shared/SignUp/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            <p>This is signUp Page</p>
+        </div>
+    );
+};
+
+export default SignUp;
